3ccd7e5cc4c823f8e3752d5d36cfb299e84b7be3
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-24 11:19:12 +0300

    Create categories page

---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,21 @@
+import { getCategories } from "../models/queries.js";
+import customError from "../helpers/customError.js";
+
+export default {
+    async getCategories(req, res, next) {
+        try {
+            const categories = await getCategories();
+            
+            res.render("pages/categories", { categories });
+        } catch (error) {
+            console.error(error);
+            next(
+                new customError(
+                    "Internal Server Error",
+                    "Something unexpected has occurred. Try reloading the page.",
+                    500,
+                ),
+            );
+        }
+    }
+};
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import controller from "../controllers/categoriesController.js";
 
 const router = Router();
 
-router.get("/");
+router.get("/", controller.getCategories);
 router.route("/new").get().post();
 router.get("/:category");
-router.route("/:category/update").get().put();
-router.route("/category/delete").get().delete();
+router.route("/:category/edit").get().put();
+router.route("/:category/delete").get().delete();
 router.route("/:category/new").get().post();
 
 export default router;
