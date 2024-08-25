2d145c5555d3e0f1c830addddd7453d7d6a57323
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-25 12:16:03 +0300

    Create validation function

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,6 +1,25 @@
-import { getCategories, addCategory, doesCategoryExist} from "../models/queries.js";
+import {
+    getCategories,
+    addCategory,
+    doesCategoryExist,
+} from "../models/queries.js";
 import customError from "../helpers/customError.js";
 import { body, validationResult, matchedData } from "express-validator";
+
+function categoryNameValidationChain() {
+    return body("name")
+        .trim()
+        .customSanitizer((value) => value.replaceAll(/\s+/g, " "))
+        .notEmpty()
+        .withMessage("Name must not be empty")
+        .isLength({ max: 16 })
+        .withMessage("Name length must be within 16 characters")
+        .custom(async (value) => {
+            if (await doesCategoryExist(value)) {
+                throw new Error("Category name already exists");
+            }
+        });
+}
 
 export default {
     async getCategories(req, res, next) {
@@ -20,28 +39,33 @@ export default {
         }
     },
     getCategoriesNew(req, res) {
-        res.render("pages/newCategory", { errors: null });
+        res.render("pages/categoryForm", {
+            title: "New category - Inventory Application",
+            heading: "New category",
+            input: null,
+            button: "Add",
+            errors: null,
+        });
     },
     postCategoriesNew: [
-        body("name")
-            .trim()
-            .customSanitizer((value) => value.replaceAll(/\s+/g, " "))
-            .notEmpty()
-            .withMessage("Name must not be empty")
-            .isLength({ max: 16 })
-            .withMessage("Name length must be within 16 characters")
-            .custom(async (value) => {
-                if (await doesCategoryExist(value)) {
-                    throw new Error("Category name already exists");
-                }
-            }),
+        categoryNameValidationChain(),
         async (req, res, next) => {
             const result = validationResult(req);
 
             if (!result.isEmpty()) {
-                return res.render("pages/newCategory", { errors: result.array() });
+                const errors = result.array();
+
+                res.render("pages/categoryForm", {
+                    title: "New category - Inventory Application",
+                    heading: "New category",
+                    input: errors[0].value,
+                    button: "Add",
+                    errors,
+                });
+
+                return;
             }
-            
+
             try {
                 const { name } = matchedData(req);
 
