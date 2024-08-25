54d4cfe07173c9e9fa7253e4a2174d81ab36b3c5
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-25 15:55:31 +0300

    Add more queries

--- src/models/queries.js
+++ src/models/queries.js
@@ -10,10 +10,21 @@ async function addCategory(name) {
     await pool.query("INSERT INTO categories (name) VALUES ($1)", [name]);
 }
 
-async function doesCategoryExist(name) {
-    const { rows } = await pool.query("SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END AS exists FROM categories WHERE name = $1", [name]);
+async function doesCategoryExist(value) {
+    const query = `SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END AS exists FROM categories WHERE ${Number.isInteger(value) ? "id " : "name"} = $1`;
+
+    const { rows } = await pool.query(query, [value]);
 
     return rows[0].exists;
 }
 
-export { getCategories, addCategory, doesCategoryExist };
+async function getCategory(id) {
+    const { rows } = await pool.query(
+        "SELECT * FROM categories WHERE id = $1",
+        [id],
+    );
+
+    return rows[0];
+}
+
+export { getCategories, addCategory, doesCategoryExist, getCategory };
