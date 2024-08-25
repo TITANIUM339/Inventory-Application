c4ac6477b444157adbf5590a4afb2a6f778f2bed
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-25 10:22:05 +0300

    Handle new category form submission

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,11 +1,12 @@
-import { getCategories } from "../models/queries.js";
+import { getCategories, addCategory, doesCategoryExist} from "../models/queries.js";
 import customError from "../helpers/customError.js";
+import { body, validationResult, matchedData } from "express-validator";
 
 export default {
     async getCategories(req, res, next) {
         try {
             const categories = await getCategories();
-            
+
             res.render("pages/categories", { categories });
         } catch (error) {
             console.error(error);
@@ -18,8 +19,45 @@ export default {
             );
         }
     },
-
     getCategoriesNew(req, res) {
-        res.render("pages/newCategory");
-    }
+        res.render("pages/newCategory", { errors: null });
+    },
+    postCategoriesNew: [
+        body("name")
+            .trim()
+            .customSanitizer((value) => value.replaceAll(/\s+/g, " "))
+            .notEmpty()
+            .withMessage("Name must not be empty")
+            .isLength({ max: 16 })
+            .withMessage("Name length must be within 16 characters")
+            .custom(async (value) => {
+                if (await doesCategoryExist(value)) {
+                    throw new Error("Category name already exists");
+                }
+            }),
+        async (req, res, next) => {
+            const result = validationResult(req);
+
+            if (!result.isEmpty()) {
+                return res.render("pages/newCategory", { errors: result.array() });
+            }
+            
+            try {
+                const { name } = matchedData(req);
+
+                await addCategory(name);
+
+                res.redirect("/categories");
+            } catch (error) {
+                console.error(error);
+                next(
+                    new customError(
+                        "Internal Server Error",
+                        "Something unexpected has occurred. Try reloading the page.",
+                        500,
+                    ),
+                );
+            }
+        },
+    ],
 };
