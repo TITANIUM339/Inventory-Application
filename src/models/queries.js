542e66de6cc94be382818b53229f634f15f3fa0f
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-25 10:19:52 +0300

    Add more query functions

--- src/models/queries.js
+++ src/models/queries.js
@@ -6,4 +6,14 @@ async function getCategories() {
     return rows;
 }
 
-export { getCategories };
+async function addCategory(name) {
+    await pool.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+}
+
+async function doesCategoryExist(name) {
+    const { rows } = await pool.query("SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END AS exists FROM categories WHERE name = $1", [name]);
+
+    return rows[0].exists;
+}
+
+export { getCategories, addCategory, doesCategoryExist };
