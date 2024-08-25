2d1f7ac51f1d386b5cf368cf820f7ca7e700bd8a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-25 16:04:26 +0300

    Validate params and add controller

--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,13 +1,38 @@
 import { Router } from "express";
 import controller from "../controllers/categoriesController.js";
+import { param, validationResult } from "express-validator";
+import { doesCategoryExist } from "../models/queries.js";
 
 const router = Router();
 
 router.get("/", controller.getCategories);
-router.route("/new").get(controller.getCategoriesNew).post(controller.postCategoriesNew);
+router
+    .route("/new")
+    .get(controller.getCategoriesNew)
+    .post(controller.postCategoriesNew);
+
+router.use("/:categoryId", [
+    param("categoryId")
+        .toInt()
+        .isInt({ min: 1 })
+        .custom(async (value) => {
+            if (!(await doesCategoryExist(value))) {
+                throw new Error();
+            }
+        }),
+    (req, res, next) => {
+        if (!validationResult(req).isEmpty()) {
+            next("router");
+            return;
+        }
+
+        next();
+    },
+]);
+
 router.get("/:categoryId");
-router.route("/:categoryId/edit").get().put();
-router.route("/:categoryId/delete").get().delete();
+router.route("/:categoryId/edit").get(controller.getCategoryEdit).post();
+router.route("/:categoryId/delete").get().post();
 router.route("/:categoryId/new").get().post();
 
 export default router;
