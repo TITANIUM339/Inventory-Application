2c468a382c2796b2ac5cec65c0901620870d4d75
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-01 18:22:47 +0300

    Add controller

--- src/routes/itemsRouter.js
+++ src/routes/itemsRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import controller from "../controllers/itemsController.js";
 
 const router = Router();
 
-router.get("/");
+router.get("/", controller.getItems);
 router.get("/:itemId");
 router.route("/:itemId/update").get().put();
 router.route("/:itemId/delete").get().delete();
