9ce9482f7b9434c4732b524b9ac4f257d758bf12
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-03 15:33:44 +0300

    Add item controller

--- src/routes/itemsRouter.js
+++ src/routes/itemsRouter.js
@@ -1,10 +1,32 @@
 import { Router } from "express";
 import controller from "../controllers/itemsController.js";
+import { param, validationResult } from "express-validator";
+import { doesItemExist } from "../models/queries.js";
 
 const router = Router();
 
 router.get("/", controller.getItems);
-router.get("/:itemId");
+
+router.use("/:itemId", [
+    param("itemId")
+        .isInt({ min: 1 })
+        .toInt()
+        .custom(async (value) => {
+            if (!(await doesItemExist(value))) {
+                throw new Error();
+            }
+        }),
+    (req, res, next) => {
+        if (!validationResult(req).isEmpty()) {
+            next("router");
+            return;
+        }
+
+        next();
+    },
+]);
+
+router.get("/:itemId", controller.getItem);
 router.route("/:itemId/update").get().put();
 router.route("/:itemId/delete").get().delete();
 
